302e3cb1dc7de4a552629974a6ff23ea1d0cc0df
Hemant Singh <hemantsingh@Hemants-MacBook-Air.local>
2021-05-01 12:35:29 +0530

    check issue fix

--- ng-checklist/src/app/checklist/checklist-list/checklist-list-item.component.ts
+++ ng-checklist/src/app/checklist/checklist-list/checklist-list-item.component.ts
@@ -1,12 +1,14 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { ChecklistItem } from '../models/checklist.model';
 
 @Component({
   selector: 'ac-checklist-list-item',
   templateUrl: './checklist-list-item.component.html',
-  styleUrls: ['./checklist-list-item.component.scss']
+  styleUrls: ['./checklist-list-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ChecklistListItemComponent {
+  allComplete = true;
   @Input() item: ChecklistItem;
   @Output() toggleItem = new EventEmitter<ChecklistItem>();
   @Output() toggleFavorite = new EventEmitter<ChecklistItem>();
